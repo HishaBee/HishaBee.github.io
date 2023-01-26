ab157e8c1db2feeddeda5073f52908ca06c97ee8
saminBee <91384773+saminBee@users.noreply.github.com>
2023-01-26 16:31:05 +0600

    updated file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -101,9 +101,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7ce15fb3b9ab5dce2099a3904cdc7fc5",
-"/": "7ce15fb3b9ab5dce2099a3904cdc7fc5",
-"main.dart.js": "8ec24e1a7669b4f5a19184bf011469a7",
+"index.html": "52f389db8409dc8e1fbf2d4c262c37b7",
+"/": "52f389db8409dc8e1fbf2d4c262c37b7",
+"main.dart.js": "70aafd8fc6e48ddf84e3ad8ed1988250",
 "manifest.json": "86d1b0846067918db9f8093fd20b4a1a",
 "version.json": "555f401091b404cdad1bd05fa1bdf257"
 };
