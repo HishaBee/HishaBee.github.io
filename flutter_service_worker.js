cc72840c27112ac67bdc1baa7f5fd8e96e9254b1
saminBee <91384773+saminBee@users.noreply.github.com>
2023-01-31 15:01:29 +0600

    timer changed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -100,9 +100,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5a13fd88adb4667c6428f4c0c775125f",
-"/": "5a13fd88adb4667c6428f4c0c775125f",
-"main.dart.js": "16fe3d2a4cff9a76a1f8015a61500d47",
+"index.html": "c14fde7be163c50f9ed29ef9c6a5cd2b",
+"/": "c14fde7be163c50f9ed29ef9c6a5cd2b",
+"main.dart.js": "65eaa618b06274c5ac363034a8aea949",
 "manifest.json": "86d1b0846067918db9f8093fd20b4a1a",
 "version.json": "555f401091b404cdad1bd05fa1bdf257"
 };
